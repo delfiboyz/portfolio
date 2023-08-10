170cc2735cb6d92548ab7314aeaab1c46168473e
delfi boyz <delfiboyz@gmail.com>
2023-08-10 21:22:29 +0700

    commit

---
+++ js/scripts.js
@@ -0,0 +1,195 @@
+/*!
+* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+$('document').ready(function () {
+    $('body').css("background-color", "aqua");
+    $("#languages-javascript").css("background-color", "lightgreen");
+    $("#languages-html").css("background-color", "lightgreen");
+    $("#languages-css").css("background-color", "lightgreen");
+    $("#languages-python").css("background-color", "lightgreen");
+    $("#languages-php").css("background-color", "lightgreen");
+    $("#languages-csharp").css("background-color", "lightgreen");
+    $("#frontend").css("background-color", "lightgreen");
+    $("#backend").css("background-color", "lightgreen");
+    $("#datascience").css("background-color", "lightgreen");
+    $("#gameprogramming").css("background-color", "lightgreen");
+    $("#machinelearning").css("background-color", "lightgreen");
+    $("#neuralnetwork").css("background-color", "lightgreen");
+
+    $("nav").mouseenter(function () {
+        $("nav").css("font-size", "25px");
+    }).mouseleave(function () {
+        $("nav").css("font-size", "16px");
+    });
+
+    $("#languages").mouseenter(function () {
+        $("#languages").css("font-size", "25px");
+
+    }).mouseleave(function () {
+        $("#languages").css("font-size", "16px");
+
+
+    });
+
+    $("#languages-javascript").mouseenter(function () {
+        $("#languages-javascript").css("background-color", "yellow");
+
+    }).mouseleave(function () {
+        $("#languages-javascript").css("background-color", "lightgreen");
+
+
+    });
+
+    $("#languages-html").mouseenter(function () {
+        $("#languages-html").css("background-color", "skyblue");
+
+    }).mouseleave(function () {
+        $("#languages-html").css("background-color", "lightgreen");
+
+
+    });
+
+    $("#languages-css").mouseenter(function () {
+        $("#languages-css").css("background-color", "orange");
+
+    }).mouseleave(function () {
+        $("#languages-css").css("background-color", "lightgreen");
+
+
+    });
+
+    $("#languages-python").mouseenter(function () {
+        $("#languages-python").css("background-color", "yellow");
+
+    }).mouseleave(function () {
+        $("#languages-python").css("background-color", "lightgreen");
+
+
+    });
+
+    $("#languages-csharp").mouseenter(function () {
+        $("#languages-csharp").css("background-color", "skyblue");
+
+    }).mouseleave(function () {
+        $("#languages-csharp").css("background-color", "lightgreen");
+
+
+    });
+
+    $("#languages-php").mouseenter(function () {
+        $("#languages-php").css("background-color", "orange");
+
+    }).mouseleave(function () {
+        $("#languages-php").css("background-color", "lightgreen");
+
+
+    });
+
+
+    $("#skills").mouseenter(function () {
+        $("#skills").css("font-size", "25px");
+
+    }).mouseleave(function () {
+        $("#skills").css("font-size", "16px");
+
+
+    });
+
+    $("#machinelearning").mouseenter(function () {
+        $("#machinelearning").css("background-color", "yellow");
+
+    }).mouseleave(function () {
+        $("#machinelearning").css("background-color", "lightgreen");
+
+
+    });
+
+    $("#frontend").mouseenter(function () {
+        $("#frontend").css("background-color", "skyblue");
+
+    }).mouseleave(function () {
+        $("#frontend").css("background-color", "lightgreen");
+
+
+    });
+
+    $("#backend").mouseenter(function () {
+        $("#backend").css("background-color", "orange");
+
+    }).mouseleave(function () {
+        $("#backend").css("background-color", "lightgreen");
+
+
+    });
+
+    $("#datascience").mouseenter(function () {
+        $("#datascience").css("background-color", "yellow");
+
+    }).mouseleave(function () {
+        $("#datascience").css("background-color", "lightgreen");
+
+
+    });
+
+    $("#neuralnetwork").mouseenter(function () {
+        $("#neuralnetwork").css("background-color", "skyblue");
+
+    }).mouseleave(function () {
+        $("#neuralnetwork").css("background-color", "lightgreen");
+
+
+    });
+
+    $("#gameprogramming").mouseenter(function () {
+        $("#gameprogramming").css("background-color", "orange");
+
+    }).mouseleave(function () {
+        $("#gameprogramming").css("background-color", "lightgreen");
+
+
+    });
+
+   /* $("#experience1").click(function () {
+        $("#experience1").css("font-size", "25px");
+
+    }).mouseleave(function () {
+        $("#experience1").css("font-size", "16px");
+
+
+    });
+
+    $("#experience2").mouseenter(function () {
+        $("#experience2").css("font-size", "25px");
+
+    }).mouseleave(function () {
+        $("#experience2").css("font-size", "16px");
+
+
+    }); */
+
+    $("#card1").mouseenter(function () {
+        $("#card1").css("background-color", "lightblue");
+
+    }).mouseleave(function () {
+        $("#card1").css("background-color", "lightgray");
+
+
+    });
+
+    $("#card2").mouseenter(function () {
+        $("#card2").css("background-color", "lightblue");
+
+    }).mouseleave(function () {
+        $("#card2").css("background-color", "lightgray");
+
+
+    });
+
+
+
+});
